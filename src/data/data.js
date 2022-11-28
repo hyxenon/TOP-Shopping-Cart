9448a2c8949c4114f3793d046a8a9e50c9ac6b17
Justine Edward Santos <hydxenonmain@gmail.com>
2022-11-28 14:18:23 +0800

    Delete Unnecessary Boilerplate, Add Tailwind CSS and Data Items

---
+++ src/data/data.js
@@ -0,0 +1,55 @@
+const data = [
+  {
+    section: "Men",
+    items: [
+      {
+        name: "Jordan 1 Retro High OG Chicago Lost and Found",
+        price: 280,
+        img: "https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1665691099",
+      },
+      {
+        name: "Jordan 4 Retro Midnight Navy",
+        price: 247,
+        img: "https://images.stockx.com/images/Air-Jordan-4-Retro-White-Midnight-Navy-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1667401281",
+      },
+      {
+        name: "Jordan 11 Retro Cherry (2022)",
+        price: 264,
+        img: "https://images.stockx.com/images/Air-Jordan-11-Retro-Cherry-2022-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1669100668",
+      },
+      {
+        name: "Jordan 4 Retro A Ma Maniére Violet Ore",
+        price: 268,
+        img: "https://images.stockx.com/images/Air-Jordan-4-Retro-A-Ma-Maniere-Violet-Ore-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1668668640",
+      },
+    ],
+  },
+
+  {
+    section: "Women",
+    items: [
+      {
+        name: "Jordan 1 Low Aluminum (W)",
+        price: 115,
+        img: " https://images.stockx.com/images/Air-Jordan-1-Low-Aluminum-W-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1666162214",
+      },
+      {
+        name: "Nike Dunk Low Retro (2021) (W)",
+        price: 143,
+        img: "https://images.stockx.com/images/Nike-Dunk-Low-White-Black-2021-W-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1611766850",
+      },
+      {
+        name: "UGG Tazz Slipper Chestnut (W)",
+        price: 152,
+        img: "https://images.stockx.com/images/UGG-Tazz-Slipper-Chestnut-W-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1645037409",
+      },
+      {
+        name: "Nike Dunk High Panda (2021) (W)",
+        price: 100,
+        img: "https://images.stockx.com/images/Nike-Dunk-High-Panda-2021-W-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1638807175",
+      },
+    ],
+  },
+];
+
+export default data;
