1f4e5bf015700f409d18bb862fca2b2003bbc077
Justine Edward Santos <hydxenonmain@gmail.com>
2022-11-28 21:47:47 +0800

    Add Men and Women Page

--- src/Pages/Men.jsx
+++ src/Pages/Men.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Card from '../components/Card'
+import data from '../data/data'
 
 const Men = () => {
+
+  const MensElements = data[0].items.map(item => <Card key={item.id} {...item}/>)
+
   return (
-    <div>Men</div>
+    <main className='px-16 mt-16'>
+      <h1 className='text-primary text-4xl font-bold text-center'>Men</h1>
+      <div className='mt-28 grid grid-cols-4 gap-4'>
+      {MensElements}
+      {MensElements}
+      </div>
+     
+    </main>
   )
 }
 
--- src/Pages/Women.jsx
+++ src/Pages/Women.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Card from '../components/Card'
+import data from '../data/data'
 
 const Women = () => {
+  const WomenElements = data[1].items.map(item => <Card key={item.id} {...item}/>)
   return (
-    <div>Women</div>
+    <main className='px-16 mt-16'>
+      <h1 className='text-primary text-4xl font-bold text-center'>Women</h1>
+      <div className='mt-28 grid grid-cols-4 gap-4'>
+      {WomenElements}
+      {WomenElements}
+      </div>
+     
+    </main>
   )
 }
 
